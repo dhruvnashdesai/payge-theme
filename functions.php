8c945bcc07123e806d28881bdca2131295f88910
Dhruv Desai <dhruv@getonzuri.com>
2025-11-04 20:06:59 -0500

    found bug, adding back strategically to fix

--- functions.php
+++ functions.php
@@ -8,3 +8,16 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ */
+function payge_theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    register_nav_menus(array(
+        'primary' => esc_html__('Primary Menu', 'payge-theme'),
+        'footer'  => esc_html__('Footer Menu', 'payge-theme'),
+    ));
+}
+add_action('after_setup_theme', 'payge_theme_setup');
+
