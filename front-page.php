e877756bfe41bedee1539476c5661b20c4af4217
Dhruv Desai <dhruv@getonzuri.com>
2025-11-20 16:18:42 -0500

    update to three stack hero title

--- front-page.php
+++ front-page.php
@@ -27,8 +27,8 @@ $monthly_title = $monthly_level ? $monthly_level->name : 'Monthly Access';
             <div class="hero-content">
                 <div class="hero-text-content">
                     <h1 class="hero-line-1" data-text="POWERED BY PAYGE">
-                        <span class="word-1">POWERED</span>
-                        <span class="word-2">BY</span>
+                        <span class="word-1">POWERED</span><br class="mobile-break">
+                        <span class="word-2">BY</span><br class="mobile-break">
                         <span class="word-3">PAYGE</span>
                     </h1>
                     <h2 class="hero-line-2" data-text="PILATES INSPIRED STRENGTH MOVEMENT">PILATES INSPIRED STRENGTH MOVEMENT</h2>
